425fd0bb168ea20a4ed50a67dc7117a9969201d2
wangkaimin <xxwangkaimin@sina.com>
2020-10-22 17:54:29 +0800

    调试tsc

---
+++ lib/index.d.ts
@@ -0,0 +1,33 @@
+import BScroll from '@better-scroll/core';
+import './back-top.scss';
+export declare type BackTopOptions = Partial<BackTopConfig> | true;
+export interface BackTopConfig {
+    right: number;
+    bottom: number;
+    width: number;
+    rollDistance: number;
+    backTopImage: string;
+    bounceTime: number;
+}
+declare module '@better-scroll/core' {
+    interface CustomOptions {
+        backTop?: BackTopOptions;
+    }
+}
+export default class BackTop {
+    scroll: BScroll;
+    static pluginName: string;
+    private backTopVisible;
+    private backTopBtn;
+    options?: BackTopConfig;
+    constructor(scroll: BScroll);
+    private init;
+    private handleBScroll;
+    private handleOptions;
+    private registerHooks;
+    private createBackTopBtn;
+    private toggleBackTopBtnVisible;
+    private handleScrollDistance;
+    private handleBackTop;
+    backTopHidden(): void;
+}
