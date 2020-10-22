a5535179d7b3415e29903546bf9683ea5ff3b6f3
wangkaimin <xxwangkaimin@sina.com>
2020-10-22 17:34:44 +0800

    1、删除多余依赖 2、添加使用说明文档

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# -better-scroll-backtop
-@better-scroll-backtop
+# -better-scroll2-backtop
+转为@better-scroll 2.x写的返回顶部插件
+
+# Install
+```bash
+npm i better-scroll2-backtop -S
+```
+
+## 使用
+```js
+import BScroll from '@better-scroll/core'
+import BackTop from 'better-scroll2-backtop'
+BScroll.use(BackTop)
+
+const bs = new BScroll('.wrapper', {
+  click: true, // 使用返回顶部插件时，必须配置click为true
+  backTop: true
+})
+```
+
+## backTop 选项对象
+```html
+right
+·类型：number
+·默认值：10
+配置返回顶部按钮距离屏幕右侧距离
+
+bottom
+·类型：number
+·默认值：30
+配置返回顶部按钮距离屏幕下侧距离
+
+width
+·类型：number
+·默认值：36
+配置返回顶部按钮宽度
+
+rollDistance
+·类型：number
+·默认值：667
+配置返回顶部按钮显示时屏幕滚动的最小距离
+
+backTopImage
+·类型：string
+·默认值：require('./back-top.png')
+配置返回顶部按钮图片
+
+bounceTime
+·类型：number
+·单位：ms
+·默认值：800
+配置返回顶部的滚动时间
+```
+
+## 实例方法
+
+```html
+backTopHidden
+·介绍：隐藏返回顶部按钮
+·参数：无
+·返回值：无
+```
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
     <div id="app">
-        <div class="app-main">
-            <router-view></router-view>
+        <div class="scroll-wrapper" ref="scroll">
+            <div class="scroll-content">
+                <div class="content-item" v-for="(i, index) in 100" :key="index">{{i}}</div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import BScroll from '@better-scroll/core'
+    import BackTop from '@/plugins/back-top'
 
+    BScroll.use(BackTop)
     export default {
-        name: 'App'
+        name: 'App',
+        data() {
+            return {
+                bscroll: null
+            }
+        },
+        created() {
+            this.initBS()
+        },
+        methods: {
+            /**
+             * 初始化better-scroll
+             */
+            initBS() {
+                this.$nextTick(() => {
+                    this.bscroll = new BScroll('.scroll-wrapper', {
+                        click: true,
+                        backTop: true
+                    })
+                })
+            }
+        }
     }
 </script>
+<style lang="scss" scoped>
+    .scroll-wrapper {
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+
+        .content-item {
+            padding: 10px;
+            text-align: center;
+            border-bottom: 1px solid #eee;
+        }
+    }
+</style>
